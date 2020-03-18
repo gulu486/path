c73c4a74bad5c13da66b06f6f782573b68873446
dongrui23 <1339450327@qq.com>
2020-03-18 23:12:37 +0800

    swiper/banner

--- project/qunar/src/pages/home/components/Recommend.vue
+++ project/qunar/src/pages/home/components/Recommend.vue
@@ -2,7 +2,7 @@
 <div>
   <div class="title">热销推荐</div>
   <ul>
-    <li class="item border-bottom" v-for="item of list" :key="item.id">
+    <router-link tag="li" :to="'/detail/' + item.id" class="item border-bottom" v-for="item of list" :key="item.id">
       <img class="item-img" :src="item.imgUrl"/>
       <div class="item-info">
         <p class="item-title">{{ item.title }}</p>
@@ -10,7 +10,7 @@
         <button class="item-button">查看详情</button>
       </div>
       <p></p>
-    </li>
+    </router-link>
   </ul>
 </div>
 </template>
