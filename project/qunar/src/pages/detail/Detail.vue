ac3d4d2131da374680ba70706500a2156745d8eb
dongrui23 <1339450327@qq.com>
2020-03-23 23:20:05 +0800

    detail-ajax exclude scrollBehavior

--- project/qunar/src/pages/detail/Detail.vue
+++ project/qunar/src/pages/detail/Detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner :sightName="sightName" :bannerImg="bannerImg" :gallaryImgs="gallaryImgs"></detail-banner>
     <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
 import DetailList from './components/List'
@@ -16,29 +17,39 @@ export default {
   name: 'Detail',
   data () {
     return {
-      list: [{
-        title: '成人票',
-        children: [{
-          title: '成人三馆联票',
-          children: [{
-            title: '成人三馆联票-某一连锁店'
-          }]
-        }, {
-          title: '成人五馆联票'
-        }]
-      }, {
-        title: '学生票'
-      }, {
-        title: '儿童票'
-      }, {
-        title: '特惠票'
-      }]
+      list: [],
+      sightName: '',
+      gallaryImgs: [],
+      bannerImg: ''
     }
   },
   components: {
     DetailBanner,
     DetailHeader,
     DetailList
+  },
+  mounted () {
+    this.getDetailInfo()
+  },
+  methods: {
+    getDetailInfo () {
+      // axios.get('/api/detail.json?id=' + this.$route.params.id)
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.getDataSucc)
+    },
+    getDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.list = data.categoryList
+        this.gallaryImgs = data.gallaryImgs
+        this.bannerImg = data.bannerImg
+      }
+    }
   }
 }
 </script>
