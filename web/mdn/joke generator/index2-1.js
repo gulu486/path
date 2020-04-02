63dd0d260751159f35cc61821ad3718e9b51b1eb
dongrui23 <1339450327@qq.com>
2020-04-02 22:39:28 +0800

    mdn

--- web/mdn/joke generator/index2-1.js
+++ web/mdn/joke generator/index2-1.js
@@ -1,16 +1,24 @@
+// id为cn监听点击事件,箭头函数 es6
 document.getElementById('cn').onclick = () => {
+  // 页签栏标题
   document.title = '笑话生成器';
+
+  // 替换文本内容
   document.getElementById('lbl-customname').textContent = '请输入自定义的名字：';
   document.getElementById('lbl-cn').textContent = '中国';
   document.getElementById('lbl-us').textContent = '美国';
   document.getElementById('lbl-uk').textContent = '英国';
+  // iuput框默认文本
   document.getElementById('customname').placeholder = '李雷';
   document.querySelector('.randomize').textContent = '随机生成笑话';
 };
 
+// id为us/uk监听点击事件,箭头函数 es6
 document.getElementById('us').onclick =
   document.getElementById('uk').onclick = () => {
     document.title = 'Silly story generator';
+
+    // 替换文本内容
     document.getElementById('lbl-customname').textContent = 'Enter custom name:';
     document.getElementById('lbl-cn').textContent = 'CN';
     document.getElementById('lbl-us').textContent = 'US';
--- web/mdn/joke generator/index2.js
+++ web/mdn/joke generator/index2.js
@@ -2,6 +2,7 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
+// 返回随机数组选项
 function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
